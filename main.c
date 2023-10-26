3a69f65d294e50ebc02e9723f3fd77d4b3f3c23f
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2023-10-25 21:34:19 -0300

    refatorando-código

--- main.c
+++ main.c
@@ -1,34 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <biblioteca.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-/*
-int main(int argc, char *argv[]) {
-	int numero1;
-	int numero2;
-	int total;
-	printf("digite um valor para o numero 1:\n");
-	scanf("%d",&numero1);
-	printf("digite um valor para o numero 2:\n");
-	scanf("%d",&numero2);
-	total = numero1+numero2;
-	if(total>10){
-		printf("Valor maior que 10");
-	}
-	else{
-		if(total<0){
-		printf("numero negativo");
-		}
-		else {
-		printf("Valor menor ou igual que 10");
-	    }
-	}
-	printf("Boa tarde, aula hje e sobre variaveis e estrutura de condicao");
-	printf("\nA soma do numero1:%d + numero2:%d resultou em %d",numero1,numero2,total);
-	return 0;
-}
-*/
 
 double multiplicar(int num1, int num2){
 	double resultado = 1;
@@ -38,9 +12,9 @@ double multiplicar(int num1, int num2){
 		return resultado;
 	}
 	
-	printf("Digite o primeiro número que deseja multiplicar\n");
+	printf("Digite o primeiro nÃºmero que deseja multiplicar\n");
 	scanf("%d", &num1);
-	printf("Digite o segundo número que deseja multiplicar\n");
+	printf("Digite o segundo nÃºmero que deseja multiplicar\n");
 	scanf("%d", &num2);
 	resultado = num1 * num2;
 	return(resultado);
@@ -55,9 +29,9 @@ double dividir(int num1, int num2){
 		return resultado;
 	}
 	
-	printf("Digite o primeiro número que deseja dividir\n");
+	printf("Digite o primeiro nÃºmero que deseja dividir\n");
 	scanf("%d", &num1);
-	printf("Digite o segundo número que deseja dividir\n");
+	printf("Digite o segundo nÃºmero que deseja dividir\n");
 	scanf("%d", &num2);
 	resultado = num1 / num2;
 	return(resultado);
@@ -67,9 +41,9 @@ double dividir(int num1, int num2){
 int somar(int num1, int num2){
 	int resultado;
 	
-	printf("Digite o primeiro número que deseja somar\n");
+	printf("Digite o primeiro nÃºmero que deseja somar\n");
 	scanf("%d", &num1);
-	printf("Digite o segundo número que deseja somar\n");
+	printf("Digite o segundo nÃºmero que deseja somar\n");
 	scanf("%d", &num2);
 	resultado = num1 + num2;
 	return(resultado);
@@ -79,9 +53,9 @@ int somar(int num1, int num2){
 int subtrair(int num1, int num2){
 	int resultado;
 	
-	printf("Digite o primeiro número que deseja subtrair\n");
+	printf("Digite o primeiro nÃºmero que deseja subtrair\n");
 	scanf("%d", &num1);
-	printf("Digite o segundo número que deseja subtrair\n");
+	printf("Digite o segundo nÃºmero que deseja subtrair\n");
 	scanf("%d", &num2);
 	resultado = num1 - num2;
 	return(resultado);
@@ -89,11 +63,12 @@ int subtrair(int num1, int num2){
 }
 
 int main(int argc, char *argv[]){
-	int opcao = 10;
-	int num1=1, num2=2, resultado;
+	int opcao;
+	int num1, num2, resultado;
 	printf("Bem vindo a calculadora do teteu!\n");
-	
-	while (opcao != 0){
+
+
+	do{
 		printf("************* Qual operacao voce deseja fazer? *************\n");
 		printf("1 - Multiplicacao\n");
 		printf("2 - Divisao\n");
@@ -125,5 +100,5 @@ int main(int argc, char *argv[]){
 				printf("O resultado da soma e: %d\n", resultado);
 			break;
 		}
-	}
+	} while(opcao != 0);
 }
