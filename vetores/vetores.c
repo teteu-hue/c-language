b357d6373684efa6b6c74bff585af086f58d2f82
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2023-10-31 20:09:34 -0300

    exercicio de vetores

--- vetores/vetores.c
+++ vetores/vetores.c
@@ -3,16 +3,20 @@
 
 void main(){
 
-	int list[10];
+	int list[20];
 	int i;
-	int userGuess;	
+	int tamanhoArr = sizeof(list) / sizeof(list[0]);	
+	int j;
 
-	i = sizeof(list);
-	printf("%d\n", i);
+	for(i = 0; i < tamanhoArr; i++){
+		printf("Posição %d:", i);
+		scanf("%d", &list[i]);
+		list[i]*=2;
+	}
+
 	printf("[");
-	for(i = 0; i <= sizeof(list); i++){
-		list[i] = scanf("%d", &userGuess);
-		printf("%d", list[i]);
+	for(j = 0; j < tamanhoArr; j++){
+		printf("%d,", list[j]);
 	}
 	printf("]");
 
